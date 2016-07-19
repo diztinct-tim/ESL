a62c57835f472e528bf303e57b8b235e52261f27
Tim <tim@diztinct.com>
2016-07-19 17:12:40 -0400

    moved to PP

--- assets/js/page-manager.js
+++ assets/js/page-manager.js
@@ -1,14 +1,41 @@
 export default class PageManager {
     before(next) {
         next();
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     loaded(next) {
         next();
+
+        $("h5.footer-info-heading").on("click", function(){
+            if($(window).width() < 801){
+                $(this).toggleClass("open");
+                $(this).next("ul").slideToggle();
+            }
+        });
+
+
+
     }
 
     after(next) {
         next();
+
     }
 
     type() {
